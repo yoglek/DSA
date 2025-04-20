2b89e57f483bd4ea2221f553a7261744a150382f
Yogeswaran Sankar <yssankar@amazon.com>
2025-04-20 21:25:28 +0530

    adding maximum sum sub array problem

---
+++ problem_solvings/max_product_sub_sum.py
@@ -0,0 +1,24 @@
+### maximum sum sub array problem
+
+arr = [1,2,-3,4,5,6]
+
+### We can use the kadane's algorithm 
+
+def max_sum_subarray(arr):
+    max_sum = arr[0]
+    curr_sum = arr[0]
+
+    for i in range(1,len(arr)):
+        if curr_sum<0:
+            curr_sum = 0
+        curr_sum +=arr[i]
+        max_sum = max(max_sum,curr_sum)
+    return max_sum
+print(max_sum_subarray(arr))
+
+## time complexity linear so O(N)
+## Space Complexity O(1) bcs its constant time 
+
+
+
+
